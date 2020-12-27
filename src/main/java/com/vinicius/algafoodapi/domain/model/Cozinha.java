380544943783bc35a2480680d4712180529996c4
Vinicius Pascucci <vinicius.pascucci1@gmail.com>
2020-12-27 14:49:10 -0300

    Iniciando Projeto Especialista Spring Rest + Criação Classes Domain

---
+++ src/main/java/com/vinicius/algafoodapi/domain/model/Cozinha.java
@@ -0,0 +1,122 @@
+package com.vinicius.algafoodapi.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Classe Cozinha que irá representar uma tabela na base de dados algafood.
+ * @author Vinicius-PC - Vinicius Torres Pascucci.
+ */
+@Entity
+public class Cozinha implements Serializable{
+
+	/**
+	 * Serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Representa o identificador da cozinha.
+	 */
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	/**
+	 * Representa o nome da cozinha.
+	 */
+	@Column( name = "nome_cozinha")
+	private String nome;
+	
+	/* Construtor default*/
+	public Cozinha() { }
+
+	/**
+	 * Construtor com argumenyos.
+	 * @param id - Long - identificador da cozinha.
+	 * @param nome - String - nome da cozinha
+	 */
+	public Cozinha(Long id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	/**
+	 * Metogo get().
+	 * @return id - Long - identificador da cozinha.
+	 */
+	public Long getId() {
+		return this.id;
+	}
+
+	/**
+	 * Metodo set().
+	 * @param id - Long - identificador da cozinha.
+	 */
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Metodo get().
+	 * @return nome - String - nome da cozinha.
+	 */
+	public String getNome() {
+		return this.nome;
+	}
+
+	/**
+	 * Metodo set().
+	 * @param nome - String - nome da cozinha.
+	 */
+	public void setNome(final String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * Metodo hasCode comparando apenas por ID.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * Metodo equals comparando se um ID é igual a outro.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cozinha other = (Cozinha) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Metodo toString().
+	 */
+	@Override
+	public String toString() {
+		return "Cozinha "
+				+ "[IDENTIFICADOR: " + id + ", \n"
+				+ "NOME DA COZINHA: " + nome + "]";
+	}	
+}
---
+++ src/main/java/com/vinicius/algafoodapi/domain/model/Restaurante.java
@@ -0,0 +1,141 @@
+package com.vinicius.algafoodapi.domain.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+* Classe Restaurante que irá representar uma tabela na base de dados algafood.
+* @author Vinicius-PC - Vinicius Torres Pascucci.
+*/
+@Entity
+public class Restaurante implements Serializable {
+
+	/**
+	 * Serial version UID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY )
+	private Long id;
+	
+	private String nome;
+
+	@Column( name = "taxa_frete")
+	private BigDecimal taxaFrete;
+	
+	/* Construtor default*/
+	public Restaurante() { }
+
+	/**
+	 * Construtor com argumentos.
+	 * @param id - Long - identificador restaurante.
+	 * @param nome - String - nome do restaurante.
+	 * @param taxaFrete - BigDecimal - taxa do frete.
+	 */
+	public Restaurante(Long id, String nome, BigDecimal taxaFrete) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.taxaFrete = taxaFrete;
+	}
+
+	/**
+	 * Metodo get().
+	 * @return id - Long - identificador do restaurante.
+	 */
+	public Long getId() {
+		return this.id;
+	}
+
+	/**
+	 * Metodo set().
+	 * @param id - Long - identificador do restaurante.
+	 */
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * Metodo get().
+	 * @return nome - String - nome do restaurante.
+	 */
+	public String getNome() {
+		return this.nome;
+	}
+
+	/**
+	 * Metodo set().
+	 * @param nome - String - nome do restaurante.
+	 */
+	public void setNome(final String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * Metodo get().
+	 * @return taxaFrete - BigDecimal - taxa do frete.
+	 */
+	public BigDecimal getTaxaFrete() {
+		return this.taxaFrete;
+	}
+
+	/**
+	 * Metodo set().
+	 * @param taxaFrete - BigDecimal - taxa do frete.
+	 */
+	public void setTaxaFrete(final BigDecimal taxaFrete) {
+		this.taxaFrete = taxaFrete;
+	}
+
+	/**
+	 * Metodo hasCode comparando apenas por ID.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/**
+	 * Metodo equals comparando se um ID é igual a outro.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurante other = (Restaurante) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Metodo toString().
+	 */
+	@Override
+	public String toString() {
+		return "Restaurante "
+				+ "[IDENTIFICADOR: " + id + ", \n"
+				+ "NOME DO RESTAURANTE: " + nome + ", \n"
+				+ "TAXA FRETE: " + taxaFrete + "]";
+	}
+	
+	
+	
+}
